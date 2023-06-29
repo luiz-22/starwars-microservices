7f6aa9ffd8848db93771dd487b81ec993d40645c
Luis <luismanlaz@hotmail..com>
2023-06-29 19:31:58 -0300

    29/06/2023

--- characters/src/server.js
+++ characters/src/server.js
@@ -8,8 +8,17 @@ server.use(express.json());
 
 server.use(require('./routes'));
 
+// Aca podemos agregar un ClientError
 server.use("*", (req, res) => {
     res.status(400).send("Not Found");
+})
+
+// Estamos como sobreescribiendo el manejado de errores
+server.use((err, req, res, next) => {
+    res.status(err.statusCode || 500).send({
+        error: true,
+        message: err.message
+    });
 })
 
 module.exports = server;
