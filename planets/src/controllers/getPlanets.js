49a2e5bbe75584ca5b9d1d515911ad9c9784b91a
Luis <luismanlaz@hotmail..com>
2023-06-29 13:03:51 -0300

    29/06/2023

--- planets/src/controllers/getPlanets.js
+++ planets/src/controllers/getPlanets.js
@@ -1,6 +1,7 @@
 const Planets = require("../data")
+const {response} = require("../utils")
 
 module.exports = async (req, res) => {
     const planets = await Planets.list()
-    res.status(200).json(planets);
+    response(res, 200, planets)
 };
