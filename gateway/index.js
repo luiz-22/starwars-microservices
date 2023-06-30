2ee20070b5949d644e2925d454eac0a809ad2044
Luis <luismanlaz@hotmail..com>
2023-06-30 16:26:39 -0300

    30/06/2023

--- gateway/index.js
+++ gateway/index.js
@@ -8,7 +8,7 @@ app.use(morgan("dev"));
 
 // Quiero que me redirija esta petición al puerto 8001 de la computadora characters
 app.use("/characters", createProxyMiddleware({
-    target: "http://localhost:8001",
+    target: "http://characters:8001",
     changeOrigin: true,
     pathRewrite: {
       "^/characters": ""
@@ -16,7 +16,7 @@ app.use("/characters", createProxyMiddleware({
 }));
 
 app.use("/films", createProxyMiddleware({
-    target: "http://localhost:8002",
+    target: "http://films:8002",
     changeOrigin: true,
     pathRewrite: {
       "^/films": ""
@@ -24,7 +24,7 @@ app.use("/films", createProxyMiddleware({
 }));
 
 app.use("/planets", createProxyMiddleware({
-    target: "http://localhost:8003",
+    target: "http://planets:8003",
     changeOrigin: true,
     pathRewrite: {
       "^/planets": ""
