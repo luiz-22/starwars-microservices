2dc652b5c411eb31c990e1f4e69d8f6f08d9d545
Luis <luismanlaz@hotmail..com>
2023-07-03 20:32:39 -0300

    03/07/2023

--- database/src/config/envs.js
+++ database/src/config/envs.js
@@ -1,5 +1,5 @@
-require("dotenv").config();
-//require("dotenv").config({path: "../../.env"});
+// require("dotenv").config();
+require("dotenv").config({path: "../../.env"});
 
 module.exports = {
     MONGO_URI: process.env.MONGO_URI,
